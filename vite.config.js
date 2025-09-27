a02b71ff6a2bd70666c47f3ddbdd2c94fc443e5f
Renz <webcraftdev.ph@gmail.com>
2025-09-27 09:47:40 +0800

    Small changes

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,8 @@ export default defineConfig({
                 //
                 'resources/js/sweetalert.js',
                 'resources/js/permissions.js',
-                'resources/js/datatables.js',
+                'resources/js/permissions.js',
+                'resources/js/permissions-manage.js',
                 // Resources paths 
                 'resources/sass/app.scss', 
                 'resources/js/app.js',
