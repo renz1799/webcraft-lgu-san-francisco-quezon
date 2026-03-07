b9b4b690519f41ac61ddc2a7932ca0343c10ad1c
Renz <webcraftdev.ph@gmail.com>
2026-03-07 19:27:46 +0800

    remove unused template files and dead controllers

--- resources/views/layouts/components/landing/styles.blade.php
+++
@@ -1,9 +0,0 @@
-
-        <!-- SIMPLEBAR CSS -->
-        <link href="{{asset('build/assets/libs/simplebar/simplebar.min.css')}}" rel="stylesheet"> 
-
-        <!-- COLOR PICKER CSS -->
-        <link rel="stylesheet" href="{{asset('build/assets/libs/@simonwep/pickr/themes/nano.min.css')}}">
-
-        <!-- SWIPER CSS -->
-        <link rel="stylesheet" href="{{asset('build/assets/libs/swiper/swiper-bundle.min.css')}}">
