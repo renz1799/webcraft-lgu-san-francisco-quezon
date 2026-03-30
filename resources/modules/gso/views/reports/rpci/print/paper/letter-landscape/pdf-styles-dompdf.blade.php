353f1e9287169671fa2e09f6e0f94ccd018f6191
Renz <webcraftdev.ph@gmail.com>
2026-03-30 18:37:35 +0800

    Dompdf spacing fix

--- resources/modules/gso/views/reports/rpci/print/paper/letter-landscape/pdf-styles-dompdf.blade.php
+++ resources/modules/gso/views/reports/rpci/print/paper/letter-landscape/pdf-styles-dompdf.blade.php
@@ -1,3 +1,67 @@
-@include('gso::reports.rpci.print.paper.a4-landscape.pdf-styles-dompdf', [
+@include('gso::reports.rpci.print.paper.letter-landscape.pdf-styles', [
     'paperProfile' => $paperProfile,
 ])
+
+<style>
+    .gso-rpci-print-page {
+        position: relative !important;
+    }
+
+    .gso-rpci-print-page__body {
+        width: auto !important;
+        padding: 3mm 9mm 21mm !important;
+    }
+
+    .gso-rpci-print-appendix {
+        padding-right: 1mm;
+        font-size: 9px !important;
+    }
+
+    .gso-rpci-print-title {
+        font-size: 14px !important;
+        margin-bottom: 3px !important;
+    }
+
+    .gso-rpci-print-flow-note,
+    .gso-rpci-print-page-number,
+    .gso-rpci-print-signature-caption {
+        font-size: 9px !important;
+    }
+
+    .gso-rpci-print-sheet th,
+    .gso-rpci-print-sheet td {
+        padding: 1px 3px !important;
+        font-size: 10px !important;
+    }
+
+    .gso-rpci-print-meta-row td,
+    .gso-rpci-print-data-row td,
+    .gso-rpci-print-fill-row td,
+    .gso-rpci-print-summary-head th,
+    .gso-rpci-print-summary-values td,
+    .gso-rpci-print-column-head th,
+    .gso-rpci-print-signatures-head th {
+        height: 13px !important;
+    }
+
+    .gso-rpci-print-signature-line {
+        width: 88% !important;
+    }
+
+    .gso-rpci-print-signature-cell {
+        min-height: 20mm !important;
+        padding-bottom: 5px !important;
+    }
+
+    .gso-rpci-print-footer {
+        position: absolute !important;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+
+    .gso-rpci-print-footer-content {
+        padding: 1px 9mm 2px !important;
+        min-height: 10px !important;
+    }
+</style>
